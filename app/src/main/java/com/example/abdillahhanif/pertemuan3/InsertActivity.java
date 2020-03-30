168fec1d051c6216ced292595a7e98df936aeac2
Abdillah Hanif <abdillahhanif25@gmail.com>
2020-03-30 17:30:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/abdillahhanif/pertemuan3/InsertActivity.java
@@ -0,0 +1,15 @@
+package com.example.abdillahhanif.pertemuan3;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+@SuppressLint("Registered")
+public class InsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+}
